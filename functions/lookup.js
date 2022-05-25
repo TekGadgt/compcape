ea16e4e0837cec6f90e0488c3c2a9bb855ff3152
Ryan McGovern <TekGadgt@users.noreply.github.com>
2022-05-25 10:09:32 -0400

    Update lookup.js

--- functions/lookup.js
+++ functions/lookup.js
@@ -10,11 +10,6 @@ exports.handler = async function (event, context) {
             database_id: process.env.NOTION_TEKGADGT,
             sorts: [{ property: "Name", direction: "ascending" }],
         })
-    } else if (event.queryStringParameters.username === 'jsleek21') {
-        var myPage = await notion.databases.query({
-            database_id: process.env.NOTION_JSLEEK21,
-            sorts: [{ property: "Name", direction: "ascending" }],
-        })
     }
 
     return {
